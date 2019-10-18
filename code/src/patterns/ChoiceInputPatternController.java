4436ccac000934246ffe04c0afe989fc933f08d7
wrackspurt <51002852+wrackspurt@users.noreply.github.com>
2019-10-18 22:13:37 +0300

    Update ChoiceInputPatternController.java

--- code/src/patterns/ChoiceInputPatternController.java
+++ code/src/patterns/ChoiceInputPatternController.java
@@ -1,4 +1,24 @@
 package code.src.patterns;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.text.Text;
+
 public class ChoiceInputPatternController {
+
+    @FXML
+    private ChoiceBox<?> fieldData;
+
+    @FXML
+    private Text nameOfField;
+
+    public String getFieldData() {
+        System.out.println(fieldData.getSelectionModel().getSelectedItem());
+        return fieldData.getSelectionModel().getSelectedItem().toString();
+    }
+
+    public void setParameters(String nameOfField) {
+        this.nameOfField.setText(nameOfField);
+    }
+
 }
