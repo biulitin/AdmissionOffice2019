e12b9edcd653e36dcfdb1c4672086b1b91219cd0
wrackspurt <51002852+wrackspurt@users.noreply.github.com>
2019-10-20 02:12:04 +0300

    created AddEditDeleteButtons.fxml and AddEditDeleteButtonsController.java

---
+++ code/src/patterns/AddEditDeleteButtonsController.java
@@ -0,0 +1,18 @@
+package code.src.patterns;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class AddEditDeleteButtonsController {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private Button editButton;
+
+    @FXML
+    private Button deleteButton;
+
+}
+
