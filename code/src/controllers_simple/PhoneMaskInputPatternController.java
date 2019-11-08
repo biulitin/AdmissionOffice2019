12b55db65e421ce6d2f1e3523d2c5f1b48e92822
SvKh99 <kris.alex.sveta@gmail.com>
2019-11-08 21:31:49 +0300

    add phone mask pattern

---
+++ code/src/controllers_simple/PhoneMaskInputPatternController.java
@@ -0,0 +1,33 @@
+package controllers_simple;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.text.Text;
+import controllers_simple.MaskField;
+
+public class PhoneMaskInputPatternController {
+    @FXML
+    public FlowPane flowPane;
+
+    @FXML
+    private MaskField fieldData;
+
+    @FXML
+    private Text nameOfField;
+
+    public String getFieldData() {
+        System.out.println(fieldData.getText());
+        return fieldData.getText();
+    }
+
+    public void setParameters(String nameOfField) { this.nameOfField.setText(nameOfField); }
+
+    public void setWidthHeight(Double width, Double height) {
+        this.flowPane.setPrefWidth(width);
+        this.flowPane.setPrefHeight(height);
+
+        this.fieldData.setPrefWidth(width*0.577);
+        this.fieldData.setPrefHeight(height*0.714);
+    }
+}
